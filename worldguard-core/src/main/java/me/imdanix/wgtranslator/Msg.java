cf98675569a38a3847e678acf0328b76e3c68453
Daniil Z. <idanix@list.ru>
2026-07-07 02:42:38 +0400

    I've missed a point

--- worldguard-core/src/main/java/me/imdanix/wgtranslator/Msg.java
+++ worldguard-core/src/main/java/me/imdanix/wgtranslator/Msg.java
@@ -69,7 +69,7 @@ public enum Msg {
     REGION_FLAGS_VALUE_STRING_CURRENT("Current value:"),
 
     // com.sk89q.worldguard.protection.flags.Flags
-    REGION_FLAGS_VALUE_DEFAULT_DENY("&c&lHey!&7 Sorry, but you can't %what% here"),
+    REGION_FLAGS_VALUE_DEFAULT_DENY("&c&lHey!&7 Sorry, but you can't %what% here."),
     REGION_FLAGS_VALUE_DEFAULT_ENTRYDENY("&c&lHey!&7 You are not permitted to enter this area."),
     REGION_FLAGS_VALUE_DEFAULT_EXITDENY("&c&lHey!&7 You are not permitted to leave this area."),
 
