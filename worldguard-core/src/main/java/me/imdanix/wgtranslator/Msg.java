54b1801b42db660e7eb83b73c7daf64fa0a145dc
Daniil Z. <idanix@list.ru>
2026-07-09 02:30:05 +0400

    Let's just use the same style for color code

--- worldguard-core/src/main/java/me/imdanix/wgtranslator/Msg.java
+++ worldguard-core/src/main/java/me/imdanix/wgtranslator/Msg.java
@@ -484,7 +484,12 @@ public enum Msg {
     }
 
     private static boolean isColorCode(char c) {
-        return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
+        return switch (c) {
+            case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                 'a', 'b', 'c', 'd', 'e', 'f',
+                 'A', 'B', 'C', 'D', 'E', 'F' -> true;
+            default -> false;
+        };
     }
 
     private static boolean isFormatCode(char c) {
